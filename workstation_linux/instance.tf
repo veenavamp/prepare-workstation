97f5abd6976ec4e25cad4c8e689a313bc6f83853
Sceva Jordan <sceva@Scevas-Air.attlocal.net>
2022-03-24 17:21:33 -0400

    updated linux

---
+++ workstation_linux/instance.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "workstation" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+  #for_each      = data.aws_subnet_ids.selected.ids
+
+  # the VPC subnet
+  subnet_id = tolist(data.aws_subnet_ids.public.ids)[0]
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.mykeypair.key_name
+  user_data = local.template_file_int
+
+  tags = {
+    Name = "workstation"
+    type = "terraform"
+  }
+}
+
+data "aws_vpc" "selected" {
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+data "aws_subnet_ids" "public" {
+   vpc_id = data.aws_vpc.selected.id
+
+  tags = {
+    Name = var.subnet_name
+  }
+}
+
+locals {
+   template_file_int  = templatefile("./install_docker_kubectl.tpl", {})
+}
+
